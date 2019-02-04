d5a38c43edf25b4ae09592f79734474d7c226637
kaviyarasujojo <42169920+kaviyarasujojo@users.noreply.github.com>
2019-02-04 14:43:36 +0530

    Create ap25.c

---
+++ ap25.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#define N 10                                                                                             
+   main( )                                                     
+   {                                                           
+       int i,j,n;                                              
+       float median,a[N],t;                                                                                                   
+       printf("Enter the number of items\n");                  
+       scanf("%d", &n);                                                              
+       for (i = 1; i <= n ; i++)                               
+           scanf("%f", &a[i]);                                 
+for (i = 1 ; i <= n-1 ; i++)                            
+       {
+for (j = 1 ; j <= n-i ; j++)                         
+          {                                                    
+              if (a[j] <= a[j+1])                              
+              { 
+                t = a[j];                                      
+                a[j] = a[j+1];                                 
+                a[j+1] = t;                                    
+              }                                                
+              elsecontinue ;                                     
+          }                                                    
+       } 
+if ( n % 2 == 0)                                        
+          median = (a[n/2] + a[n/2+1])/2.0 ;                   
+       else                                                    
+          median = a[n/2 + 1];                                 
+for (i = 1 ; i <= n ; i++)                              
+           printf("%f  ", a[i]);                               
+       printf(%f, median);                   
+                                                         
+   }    
