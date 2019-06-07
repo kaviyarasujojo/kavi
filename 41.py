2c026cb447500805b798dfec8d07e6864feceb38
kaviyarasujojo <42169920+kaviyarasujojo@users.noreply.github.com>
2019-06-07 12:36:49 +0530

    Update 41.py

--- 41.py
+++ 41.py
@@ -1,4 +1,9 @@
-s=input()
-n=int(input())
-for i in range(0,n):
+string=input().split()
+for i in string:
+    if(i.isdigit()):
+        c=int(i)
+for i in string:
+    if(i.isalpha()):
+        s=i
+for i in range(0,c):
     print(s)
